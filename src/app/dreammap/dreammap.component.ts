71065c5712dd1860ed496179d15a593dd401b39a
李龙辉 <lilonghui@lilonghuideMBP.lan>
2018-06-23 16:53:06 +0800

    add d3 force map examp

--- src/app/dreammap/dreammap.component.ts
+++ src/app/dreammap/dreammap.component.ts
@@ -2,12 +2,15 @@ import { Component, OnInit, ElementRef } from '@angular/core';
 
 import * as d3 from 'd3';
 
+const ngcont = '_ngcontent-c1';
+
 @Component({
   selector: 'app-dreammap',
   templateUrl: './dreammap.component.html',
   styleUrls: ['./dreammap.component.css']
 })
-export class DreammapComponent implements OnInit {
+export class DreammapComponent implements OnInit { 
+
 
 
   constructor() {
@@ -19,30 +22,41 @@ export class DreammapComponent implements OnInit {
       width = +svg.attr("width"),
       height = +svg.attr("height");
 
-    var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+    var color = d3.scaleOrdinal(d3.schemeCategory10);
 
     var simulation = d3.forceSimulation()
       .force("link", d3.forceLink().id(function(d) { return d.id; }))
       .force("charge", d3.forceManyBody())
       .force("center", d3.forceCenter(width/2, height/2));
 
-      d3.json("./miserables.json", function(error, graph) {
-        if (error) throw error;
+      d3.json("assets/miserables.json", function(error, graph) {
+        if (error) {
+          console.log('get err');
+          throw error;
+        }
+
+        console.log('run here');
 
         var link = svg.append("g")
+            .attr(ngcont, "")
             .attr("class", "links")
           .selectAll("line")
           .data(graph.links)
           .enter().append("line")
+            .attr(ngcont, "")
+            .attr("fill", color(0))
             .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
 
         var node = svg.append("g")
             .attr("class", "nodes")
+            .attr(ngcont, "")
           .selectAll("circle")
           .data(graph.nodes)
           .enter().append("circle")
+            .attr(ngcont, "")
             .attr("r", 5)
-            .attr("fill", function(d) { return color(d.group); })
+            .attr("fill", function(d) { return color(d.group%10); })
             .call(d3.drag()
                 .on("start", dragstarted)
                 .on("drag", dragged)
@@ -69,25 +83,29 @@ export class DreammapComponent implements OnInit {
               .attr("cx", function(d) { return d.x; })
               .attr("cy", function(d) { return d.y; });
         }
+
+
+        function dragstarted(d) {
+          if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+          d.fx = d.x;
+          d.fy = d.y;
+        }
+
+        function dragged(d) {
+          d.fx = d3.event.x;
+          d.fy = d3.event.y;
+        }
+
+        function dragended(d) {
+          if (!d3.event.active) simulation.alphaTarget(0);
+          d.fx = null;
+          d.fy = null;
+        }
       });
+
+      console.log('init ok');
     
   }
 
-  function dragstarted(d) {
-    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-    d.fx = d.x;
-    d.fy = d.y;
-  }
-
-  function dragged(d) {
-    d.fx = d3.event.x;
-    d.fy = d3.event.y;
-  }
-
-  function dragended(d) {
-    if (!d3.event.active) simulation.alphaTarget(0);
-    d.fx = null;
-    d.fy = null;
-  }
 
 }
