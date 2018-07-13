6638dd73c3fe108c642633bc81b88a83ba35fc94
lilh <lilh@huiepay.com>
2018-07-13 15:47:37 +0800

    add test data

---
+++ src/app/memory/wordlink/worddata.ts
@@ -0,0 +1,4 @@
+
+import {WordLink} from './wordlink';
+
+export const WORDLINK: WordLink[] = [{"Id_list":"13:袜子,12:香烟","Link_desc":"","Link_level":""},{"Id_list":"24:啤酒,9:钳子","Link_desc":"","Link_level":""},{"Id_list":"20:挂历,17:磁带","Link_desc":"","Link_level":""},{"Id_list":"10:眼睛,14:思考","Link_desc":"","Link_level":""},{"Id_list":"23:瓶子,4:别针","Link_desc":"","Link_level":""},{"Id_list":"18:公鸡,8:钟表","Link_desc":"","Link_level":""},{"Id_list":"1:椅子,11:桌子","Link_desc":"","Link_level":""},{"Id_list":"15:哭泣,21:大脑","Link_desc":"","Link_level":""},{"Id_list":"19:下雨,3:河流","Link_desc":"","Link_level":""},{"Id_list":"2:香蕉,6:电视","Link_desc":"","Link_level":""}];
--- src/app/memory/wordlink/wordlink.service.ts
+++ src/app/memory/wordlink/wordlink.service.ts
@@ -6,13 +6,17 @@ import { catchError, map, tap } from 'rxjs/operators';
 
 import {WordLink} from './wordlink';
 
+//for test data
+import {WORDLINK} from './worddata';
+
 @Injectable({
   providedIn: 'root'
 })
 export class WordlinkService {
 
   getWords(): Observable<WordLink[]> {
-    return this.http.get<WordLink[]>('http://localhost:3631/api/wordList/wordPair');
+    //return this.http.get<WordLink[]>('http://localhost:3631/api/wordList/wordPair');
+    return of(WORDLINK);
   }
 
   constructor(private http: HttpClient) { }
